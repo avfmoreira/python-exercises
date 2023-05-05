548501e63892624af22870f1fcd6f04b8b32dadd
Andre Moreira <avfmoreira@gmail.com>
2023-05-04 22:38:41 -0300

    result verification ok

--- blackjack.py
+++ blackjack.py
@@ -4,7 +4,11 @@ a black jack simple game version
 a computer dealer
 a human player
 
-
+TO-DO: 
+- check result message
+- check ace variation
+- limit hit dealer
+- ask about play again  
 '''
 
 import random
@@ -38,35 +42,7 @@ class Deck: #Construct the deck of cards from Card
     def suffle_cards(self):
         random.shuffle(self.all_cards)
 
-class Dealer: #create dealer
-
-    def __init__(self):
-        self.deck = Deck().all_cards
-        self.hand =[]
-
-    def __str__(self):
-        return f'The Dealer has {len(self.hand)} cards.'
-
-    def ask_for_bet(self):
-        answer = input('Do you want a new card? Type Y for yes or any key for no: ')
-        if "Y" == answer.upper():
-            return True
-        return False
-
-    def get_new_card(self):
-        return self.deck.pop()
-    
-    def add_card_hand(self, new_card):
-        self.hand.append(new_card)
-        return True
-    
-    def chips_values(self):
-        counter_cards_values = 0
-        for card in self.hand:
-            counter_cards_values += card.value
-        return counter_cards_values
-    
-class Player:
+class Player():
     def __init__(self, name):
 
         self.name = name
@@ -76,34 +52,61 @@ class Player:
         return f'Player {self.name} has {len(self.hand)} cards.'
     
     def add_card_hand(self, new_card):
-        #check the best value for Ace
-
-        #check if win
-
         self.hand.append(new_card)
         return True
     
-    def chips_values(self):
+    def check_chips(self):
         counter_cards_values = 0
         for card in self.hand:
             counter_cards_values += card.value
         return counter_cards_values
+    
+    def is_winner(self):
+       
+        if self.check_chips() == 21:
+            return True
+        return False
+    
+    def is_loser(self):
+        if self.check_chips() > 21:
+            return True
+        return False
 
+class Dealer(Player): #create dealer
+
+    def __init__(self):
+        self.name = 'Dealer'
+        self.deck = Deck().all_cards
+        self.hand =[]
+
+    def __str__(self):
+        return f'The Dealer has {len(self.hand)} cards.'
+
+    def ask_for_bet(self):
+        answer = input('Do you want a new card? Type Y for yes or any key for no: ')
+        print()
+        if "Y" == answer.upper():
+            return True
+        return False
+
+    def get_new_card(self):
+        return self.deck.pop()
+    
 '''
 ## Game Play
 To play a hand of Blackjack the following steps must be followed:
 
-4. Make sure that the Player's bet does not exceed their available chips
-5. Deal two cards to the Dealer and two cards to the Player
-6. Show only one of the Dealer's cards, the other remains hidden
-7. Show both of the Player's cards
-8. Ask the Player if they wish to Hit, and take another card
+
+
 9. If the Player's hand doesn't Bust (go over 21), ask if they'd like to Hit again.
 10. If a Player Stands, play the Dealer's hand. The dealer will always Hit until the Dealer's value meets or exceeds 17
 11. Determine the winner and adjust the Player's chips accordingly
 12. Ask the Player if they'd like to play again
 '''
-
+def show_results(winner_obj, loser_obj):
+    print(f'Congratulations {winner_obj.name}, YOU WIN!')
+    print(f'Your cards sum was {winner_obj.check_chips()}')
+    print(f'{loser_obj} cards sum was {loser_obj.check_chips()}')
 
 dealer = Dealer()
 player = Player("Andre")
@@ -117,17 +120,31 @@ while i < 2:
 
 playing = True
 while playing:
-    if(player.chips_values() == 21):
-        # WIIIIIIIN
-        pass
+    print(f'Your cards are:')
+    [print(card) for card in player.hand]
+    print()
+    print(f'The Dealer has {len(dealer.hand)} and one of them is {dealer.hand[-1]}')
+    print()
+    
+    if(player.is_winner()):
+        show_results(player, dealer)
         break
-    if(player.chips_values() > 21):
-        #Loooooose
-        pass
+        
+    if(dealer.is_winner()):
+        show_results(dealer, player)
         break
-
-    print(f'your cards sum {player.chips_values()}')
+    
+    if(player.is_loser()):
+        show_results(dealer, player)
+        break
+      
+    if(dealer.is_loser()):
+        show_results(player, dealer)
+        break
+    
+    
     if dealer.ask_for_bet():
         player.add_card_hand(dealer.get_new_card())
+        dealer.add_card_hand(dealer.get_new_card())
     else:
         playing = False
